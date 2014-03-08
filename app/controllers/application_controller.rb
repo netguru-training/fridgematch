43cb667303e1b33706e2c553f8438dd56544ab38
Dariusz Choruzy <darobial@gmail.com>
2014-03-08 16:34:20 +0100

    you need to be an admin to access admin panel

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,12 @@ class ApplicationController < ActionController::Base
   decent_configuration do
     strategy DecentExposure::StrongParametersStrategy
   end
+
+  def authenticate_admin_user!
+    raise SecurityError unless current_user.try(:admin?)
+  end
+
+  rescue_from SecurityError do |exception|
+    redirect_to root_url
+  end
 end
--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -55,7 +55,7 @@ ActiveAdmin.setup do |config|
   #
   # This setting changes the method which Active Admin calls
   # within the controller.
-  config.authentication_method = :authenticate_user!
+  config.authentication_method = :authenticate_admin_user!
 
   # == User Authorization
   #
@@ -101,7 +101,7 @@ ActiveAdmin.setup do |config|
   # link. For example :get, :delete, :put, etc..
   #
   # Default:
-  # config.logout_link_method = :get
+  config.logout_link_method = :delete
 
 
   # == Root
