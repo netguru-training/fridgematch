3426e54463e0199ee2e97b6bf51a5e6ac21693b4
narspeth <emildziarlowski@gmail.com>
2014-03-08 15:29:15 +0100

    Validations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,9 @@ class Recipe < ActiveRecord::Base
 
   has_and_belongs_to_many :ingredients
 
+  validates :title, presence: true, uniqueness: true
+
+  validates :url, presence: true, uniqueness: true
+
+  validates :thumbnail, presence: true, uniqueness: true
 end
