1ae5b89f93947e4b7c0b966a75cdf25969e47c14
Bartosz Kopiński <bartosz.kopinski@netguru.pl>
2014-03-09 16:13:38 +0100

    quick fix

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,8 +1,12 @@
 class RecipesController < ApplicationController
   expose_decorated(:recipes) { RecipeMatcher.new(current_user).find(10) }
+  expose(:diet){ Diet.find(params[:diet_id]) }
 
   def index
-  @diet = Diet.find(params[:diet_id])
+    if params[:diet_id]
+      self.recipes.reject do |r|
+        diet
+      end
+    end
   end
-
 end
