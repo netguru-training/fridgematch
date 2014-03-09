35e49466f22e9a3e9b7b226e675b2978e43f50e0
Lukasz Potepa <lpotepa@o2.pl>
2014-03-09 09:59:51 +0100

    improved nutritronix fetch

--- app/models/concerns/nutritronix_fetch.rb
+++ app/models/concerns/nutritronix_fetch.rb
@@ -1,8 +1,9 @@
 module NutritronixFetch
   def self.parse(name)
+    require 'open-uri'
     app_id = 'e80f9aa5'
     app_key = 'b72a672f9df789b608e73f32334957c1'
-    response = Net::HTTP.get_response("api.nutritionix.com","/v1_1/search/#{name}?fields=item_name%2Cnf_total_carbohydrate%2Cnf_total_fat%2Cnf_protein%2Cnf_calories&appId=#{app_id}&appKey=#{app_key}")
+    response = Net::HTTP.get_response("api.nutritionix.com","/v1_1/search/#{URI::encode(name)}?fields=item_name%2Cnf_total_carbohydrate%2Cnf_total_fat%2Cnf_protein%2Cnf_calories&appId=#{app_id}&appKey=#{app_key}")
     body = JSON.parse(response.body)["hits"].first['fields']
     unit = body["item_name"].split(" - ")[1]
     body["unit"] = unit
