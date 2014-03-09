ff2cab102bed496e47ed7a27bf3bb18e49a6a1c9
Bartosz Kopiński <bartosz.kopinski@netguru.pl>
2014-03-09 16:43:34 +0100

    small fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-# fetcher = RecipesFetcher.new
-# nutritionix_fetcher = NutritronixFetcher.new
-
-# (1..100).each do |i|
-#    fetcher.run(i)
-# end
-
-# ingredients = Ingredient.select { |ingredient| ingredient.name != nil }
-# ingredients.each { |ingredient| nutritionix_fetcher.fetch(ingredient)}
-
 Diet.create(
   name: "Low Carb diet",
   description: 'Carmelized Onion fritata (1 serving), Strawberries (1 cup), All American Tuna (1 serving), Simple Kale Salad (2 cup), Eye of round roast (8 ounces), Green Salad (2 cup), Cheddar cheese (2 oz), Roasted Halibut with Garlic Sauce',
