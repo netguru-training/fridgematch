6ac4754601b020c047f577760cbbf39ea2b6580a
Dariusz Choruzy <darobial@gmail.com>
2014-03-09 12:47:23 +0100

    better decorators

--- app/decorators/recipe_decorator.rb
+++ app/decorators/recipe_decorator.rb
@@ -7,7 +7,9 @@ class RecipeDecorator < Draper::Decorator
   end
 
   def ingredients_to_buy user
-    object.missing_ingredients(user).map(&:name).to_a
+    object.missing_ingredients(user).map(&:name).map do |ingredient|
+      h.link_to ingredient,"http://groceries.asda.com/asda-webstore/landing/home.shtml?cmpid=ahc-_-ghs-sna1-_-asdacom-dsk-_-hp#/search/"+ ingredient
+    end.to_sentence.html_safe
   end
 
   def percentage_score user
