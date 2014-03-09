26d0ecfd08a1123b867de1cc7f7ddc2c997935ca
Lukasz Potepa <lpotepa@o2.pl>
2014-03-09 16:10:31 +0100

    dropdown

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,3 +1,8 @@
 class RecipesController < ApplicationController
   expose_decorated(:recipes) { RecipeMatcher.new(current_user).find(10) }
+
+  def index
+  @diet = Diet.find(params[:diet_id])
+  end
+
 end
